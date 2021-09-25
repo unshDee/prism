c9e7eabf859567e9e04729db62bb93d66ed6d8c0
Abhay <60382047+abhaytiwari262000@users.noreply.github.com>
2021-09-25 13:34:59 +0530

    Create ip_recognizer.py

---
+++ ip_recognizer.py
@@ -0,0 +1,46 @@
+import re
+from entity import Entity
+from typing import List
+
+
+class IPRecognizer:
+
+    """
+   
+    """
+
+    def __init__(self, text) -> None:
+        self.text = text
+        self.entityName = "IP_ADDRESS"
+        self.entityList = []
+
+    def getRegex(self) -> None:
+
+        regexIp6 = r"\s*(?!.*::.*::)(?:(?!:)|:(?=:))(?:[0-9a-f]{0,4}(?:(?<=::)|(?<!::):)){6}(?:[0-9a-f]{0,4}(?:(?<=::)|(?<!::):)[0-9a-f]{0,4}(?:(?<=::)|(?<!:)|(?<=:)(?<!::):)|(?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)){3})\s*"
+        regexIp4 = r"\b(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b"
+        pattern = re.compile(regexIp6)
+        pattern=re.compile(regexIp4)
+
+        matches = re.findall(pattern, self.text)
+
+        for match in matches:
+            entity = Entity(
+                text=match,
+                label=self.entityName,
+                start=self.text.index(match),
+                end=self.text.index(match) + len(match),
+                sensitivityScore=0.9,
+                country="GENRIC"
+            )
+            self.entityList.append(entity)
+
+    @staticmethod
+    def validate() -> None:
+        pass
+
+    def getEntity(self) -> List:
+        self.getRegex()
+        self.validate()
+        return self.entityList
+
+
